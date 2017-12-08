3e0832ee342f961e307652049ba85d3d98ad5fd7
David Corticchiato <david.corticchiato@gmail.com>
2017-12-08 17:37:22 +0100

    use transform instead of x,y values

--- src/sokoban/character.rs
+++ src/sokoban/character.rs
@@ -7,37 +7,36 @@ use graphics::{Image, Transformed};
 use graphics::draw_state::DrawState;
 use graphics::rectangle::square;
 use graphics::context::Context;
+use graphics::math::{Matrix2d, identity, multiply};
 use sokoban::Provider;
 
 pub struct Character {
-    x: f64,
-    y: f64,
     image: Image,
     texture: Texture,
-    draw_state: DrawState
+    draw_state: DrawState,
+    transform: Matrix2d
 }
 
 impl Character {
 
     pub fn load(provider: &Provider) -> Character {
         Character {
-            x: 0.0,
-            y: 0.0,
             draw_state: DrawState::new_alpha(),
             image: Image::new().rect(square(0.0, 0.0, 50.0)),
-            texture: provider.load_texture("Character1")
+            texture: provider.load_texture("Character1"),
+            transform: identity()
         }
     }
 
     pub fn update(&mut self, dt: f64) {
-        self.x += 5.0 * dt;
+        self.transform = self.transform.trans(5.0 * dt, 0.0);
     }
 
     pub fn render(&mut self, context: &Context, gl: &mut GlGraphics) {
       self.image.draw(
           &self.texture,
           &self.draw_state,
-          context.transform.trans(self.x, self.y),
+          multiply(context.transform, self.transform),
           gl
       );
     }
